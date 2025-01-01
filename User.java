c8a8a9ff241bfd48e9774d9bbf8ad163ebe1ce27
Asyraaf Mokhtar Sani <asyraafmys@gmail.com>
2025-01-01 11:48:32 +0800

    Update User.java

--- User.java
+++ User.java
@@ -1,27 +1,25 @@
-package vbs3;
+package testVBS;
+
 
 public class User {
-    private int userId;
     private String username;
     private String password;
     private int type; // 1 = admin, 2 = user
     
     //parametrize contructor;
-    public User(int userId, String username, String password, int type){
-        this.userId = userId;
+    public User( String username, String password, int type){
         this.username = username;
         this.password = password;
         this.type = type;
     }
 
-    //getter
-    public int getUserId() {return userId;}
+    //getter}
     public String getUsername() {return username;}
     public String getPassword() {return password;}
     public int getType() {return type;}
 
     //setter
-    public void setUserId(int userId) {this.userId = userId;}
+    //public void setUserId(int userId) {this.userId = userId;}
     public void setUsername(String username) {this.username = username;}
     public void setPassword(String password) {this.password = password;}
     public void setType(int type) {this.type = type;}
